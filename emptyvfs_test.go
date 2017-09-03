88f93581cadfd157a0ccc7f5da3c0fe5609dd854
Thomas Frössman <thomasf@jossystem.se>
2017-09-03 23:24:10 +0200

    Add mapfs from godoc vfs (make empty test run)

--- emptyvfs_test.go
+++ emptyvfs_test.go
@@ -8,8 +8,8 @@ import (
 	"testing"
 	"time"
 
-	"golang.org/x/tools/godoc/vfs"
-	"golang.org/x/tools/godoc/vfs/mapfs"
+	"github.com/thomasf/vfs"
+	"github.com/thomasf/vfs/mapfs"
 )
 
 func TestNewNameSpace(t *testing.T) {
